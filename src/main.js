4ca2f2c200cc527104e5f46c46565e9cff052464
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2023-04-01 19:01:08 -0600

    Added speech recognition
    - Finished Implementing speech recognition.
    - Using tailwind for styling.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,57 @@
-// Document.
-speechText = document.querySelector('#interim-text');
+// DOM
+const startBtn = document.querySelector("#start");
+const language = document.querySelector("#endregionlanguage-select");
+const dialect = document.querySelector("#dialect-select");
+const final = document.querySelector("#final-result");
+const interim = document.querySelector("#interim-result");
 
-// Variables.
+// Variables
 const recognition = new webkitSpeechRecognition();
-let finalTranscript;
-let interimTranscript;
+let started = false;
 
-recognition.continuous = true;
+// Setup
+recognition.lang = "en-US";
+recognition.continuous = false;
 recognition.interimResults = true;
-recognition.lang = 'es-ES'
+recognition.lang = dialect.value;
 
-recognition.onresult = event => {
-    interimTranscript = "";
-    for (let i = event.resultIndex; i < event.results.length; i++) {
+// Events
+recognition.onresult = (event) => {
+    let interimTranscript = "";
+    let finalTranscript = "";
+
+    for (let i = 0; i < event.results.length; i++) {
         if (event.results[i].isFinal) {
             finalTranscript += event.results[i][0].transcript;
         } else {
             interimTranscript += event.results[i][0].transcript;
         }
     }
+
+    final.innerText = finalTranscript;
+    interim.innerText = interimTranscript;
+};
+recognition.onaudioend = () => {
+    if (!started) return;
+    handleSpeachRecogtiniton();
+}
+dialect.addEventListener('change', () => changeLang(dialect.value));
+
+// Functions
+function changeLang(dialect) {
+    recognition.lang = dialect;
 }
 
-speechText.textContent = interimTranscript;
-
-recognition.start();
+function handleSpeachRecogtiniton() {
+    if (!started) {
+        started = true
+        recognition.start();
+        startBtn.className = "bg-red-500 hover:bg-red-700 transition ease duration-300 p-2 rounded-md px-12"
+        startBtn.innerText = "Stop";
+    } else {
+        started = false
+        recognition.stop();
+        startBtn.className = "bg-green-500 hover:bg-green-700 transition ease duration-300 p-2 rounded-md px-12"
+        startBtn.innerText = "Start";
+    } 
+}
